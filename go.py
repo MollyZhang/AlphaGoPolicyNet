1a9d6a0cc63e6df3ac3a9f8c858d8336ee54430f
Molly Zhang <mollyzhang@ucsc.edu>
2016-05-23 11:55:21 -0700

    mix black and white moves into one kind of move: move of me, looking at stones of mine and stones of opponents on a board

--- go.py
+++ go.py
@@ -3,11 +3,11 @@ import numpy as np
 import pandas as pd
 
 STONE_DICT = {"empty": 0, "W": 1, "B": 2}
-
+STONE_DICT2 = {"Empty": 0, "Me": 1, "Opponent": 2}
 
 
 def main():
-    Game_Parser("Database/0196-1699/0196-00-00.sgf")
+    (features, labels) = Game_Parser("Database/0196-1699/0196-00-00.sgf")
 
 
 
@@ -25,18 +25,38 @@ def Game_Parser(gamefile):
         if line[0:3] == "AB[" or line[0:3] == "AW[":
             add_starting_stones(start_positions, line)
         elif line[0] == ";":
-            board_positions.append(start_positions)
-            all_moves += line[1:].strip().split(";")
+            all_moves += line[1:].strip().strip().split(";")
 
+    board_positions.append(start_positions)
     for move in all_moves:
         next_moves.append(parse_move(move))
         new_board_position = add_stone_to_board(board_positions[-1], next_moves[-1])
         board_positions.append(new_board_position)
 
+    # remove last board position because there is no new moves
+    board_positions.pop(-1)
 
+    # stop distingush between black and white stone by
+    # universally call the stones "my stones" and "oponent's stones"
+    features, labels = universalize_stones(board_positions, next_moves)
+    return features, labels
 
-
-
+def universalize_stones(positions, moves):
+    features = []
+    labels = []
+    for i in range(len(moves)):
+        if moves[i][0] == 1:
+            labels.append(moves[i][1])
+            features.append(positions[i])
+        elif moves[i][0] == 2:
+            labels.append(moves[i][1])
+            reversed_position = np.array(positions[i], copy=True)
+            reversed_position[reversed_position == 1] = 10
+            reversed_position[reversed_position == 2] = 1
+            reversed_position[reversed_position == 10] = 2
+        else:
+            raise Exception("a move type can't be any number other than 1 or 2")
+    return features, labels
 
 def add_stone_to_board(board, move):
     column = move[1][0]
