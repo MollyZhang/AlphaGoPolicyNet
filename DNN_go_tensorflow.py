1387e27e89b9aefebd1fc53cddc3c08b75359d1d
Molly Zhang <mollyzhang@ucsc.edu>
2016-06-06 11:06:31 -0700

    add one layer of convolutional layer and one fully connected layer

--- DNN_go_tensorflow.py
+++ DNN_go_tensorflow.py
@@ -18,24 +18,16 @@ def conv():
     x = tf.placeholder(tf.float32, shape=[None, 361])
     y_ = tf.placeholder(tf.float32, shape=[None, 361])
 
-    W_conv1 = weight_variable([5, 5, 1, 32])
-    b_conv1 = bias_variable([32])
+    W_conv1 = weight_variable([8, 8, 1, 10])
+    b_conv1 = bias_variable([10])
     x_board = tf.reshape(x, [-1, 19, 19, 1])
 
     h_conv1 = tf.nn.relu(conv2d(x_board, W_conv1) + b_conv1)
-    h_pool1 = max_pool_2x2(h_conv1)
 
-
-    W_conv2 = weight_variable([5, 5, 32, 64])
-    b_conv2 = bias_variable([64])
-
-    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-    h_pool2 = max_pool_2x2(h_conv2)
-
-    W_fc1 = weight_variable([7 * 7 * 64, 1024])
+    W_fc1 = weight_variable([19 * 19 * 10, 1024])
     b_fc1 = bias_variable([1024])
 
-    h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+    h_pool2_flat = tf.reshape(h_conv1, [-1, 19*19*10])
     h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
     keep_prob = tf.placeholder(tf.float32)
