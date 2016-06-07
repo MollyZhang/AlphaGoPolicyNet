102584ecd37c5f0cd8169391016950f7c7a48a80
Molly Zhang <mollyzhang@ucsc.edu>
2016-06-07 04:21:41 -0700

    check opening move symmetry

--- go.py
+++ go.py
@@ -10,13 +10,16 @@ import visualization as vz
 
 
 def main():
-    go_data = gp.parse_games(num_games=100, first_n_moves=1)
+    go_data = gp.parse_games(num_games='All', first_n_moves=1)
     with open("probabilitiy_of_open_game", "r") as f:
         probs = pickle.loads(f.read())
         f.close()
-    example_open_moves = go_data.train.labels
+    open_moves = go_data.train.labels
 
-    vz.draw_board(gp.map_1d_to_2d(example_open_moves[:5]), probs['All'])
+    for i in [60, 72, 288, 300]:
+        print i, len(open_moves[open_moves==i])
+
+    vz.draw_board(gp.map_1d_to_2d(open_moves[:5]), probs['All'])
 
 
 
