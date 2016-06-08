b62e0cb797c2dcb3c7ea5e002cf92e3d6abcf88c
Molly Zhang <mollyzhang@ucsc.edu>
2016-06-08 13:39:30 -0700

    add plotting method for neural network scheme

--- visualization.py
+++ visualization.py
@@ -7,21 +7,15 @@ def draw_openning_board(stones, probs):
     # create a 8" x 8" board
     fig = plt.figure(figsize=[8,8])
     fig.patch.set_facecolor((1,1,.8))
-
     ax = fig.add_subplot(111)
-
     # draw the grid
-    for x in range(19):
-        ax.plot([x, x], [0,18], 'k')
-    for y in range(19):
-        ax.plot([0, 18], [y,y], 'k')
+    for x in range(19): ax.plot([x, x], [0,18], 'k')
+    for y in range(19): ax.plot([0, 18], [y,y], 'k')
 
     # scale the axis area to fill the whole figure
     ax.set_position([0,0,1,1])
-
     # get rid of axes and everything (the figure background will show through)
     ax.set_axis_off()
-
     # scale the plot area conveniently (the board is in 0,0..18,18)
     ax.set_xlim(-1,19)
     ax.set_ylim(-1,19)
@@ -42,8 +36,6 @@ def draw_openning_board(stones, probs):
             transparency = new_probs[stone]
             s, = ax.plot(stone[1], 18-stone[0], 'o', markersize=28,
                 color = (0.75, 0, 0.75, transparency), markeredgewidth=0)
-
-
 
     plt.show()
 
@@ -93,4 +85,39 @@ def draw_board(board, move, prob):
 
 
 
+    plt.show()
+
+
+    import matplotlib.pyplot as plt
+
+
+def draw_neural_net(layer_sizes):
+    '''
+    Draw a neural network cartoon using matplotilb.
+    '''
+    fig = plt.figure(figsize=(12, 12))
+    ax = fig.gca()
+    left, right, bottom, top = 0.1, 0.9, 0.1, 0.9
+
+
+    n_layers = len(layer_sizes)
+    v_spacing = (top - bottom)/float(max(layer_sizes))
+    h_spacing = (right - left)/float(len(layer_sizes) - 1)
+    # Nodes
+    for n, layer_size in enumerate(layer_sizes):
+        layer_top = v_spacing*(layer_size - 1)/2. + (top + bottom)/2.
+        for m in xrange(layer_size):
+            circle = plt.Circle((n*h_spacing + left, layer_top - m*v_spacing), v_spacing/4.,
+                                color='w', ec='k', zorder=4)
+            ax.add_artist(circle)
+    # Edges
+    for n, (layer_size_a, layer_size_b) in enumerate(zip(layer_sizes[:-1], layer_sizes[1:])):
+        layer_top_a = v_spacing*(layer_size_a - 1)/2. + (top + bottom)/2.
+        layer_top_b = v_spacing*(layer_size_b - 1)/2. + (top + bottom)/2.
+        for m in xrange(layer_size_a):
+            for o in xrange(layer_size_b):
+                line = plt.Line2D([n*h_spacing + left, (n + 1)*h_spacing + left],
+                                  [layer_top_a - m*v_spacing, layer_top_b - o*v_spacing], c='k')
+                ax.add_artist(line)
+    ax.set_axis_off()
     plt.show()
